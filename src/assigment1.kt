ea2774f0f73fd61a1cce8dab12e2ff062a3eb17c
zillahmwimali <zillahmwimmali@gmail.com>
2021-02-20 23:26:50 +0300

    my asignment

---
+++ src/assigment1.kt
@@ -0,0 +1,15 @@
+import kotlin.test.assertTrue
+
+fun main() {
+    val name= "mitchelleMbaitsa"
+    print(name)
+    var age= 23
+    print(age)
+    var weight= 74.735432
+    print(weight)
+    var phoneNumber= 71012345678
+    print(phoneNumber)
+    val citizen= ("true")
+    val student= arrayOf("mitchelleMbaitsa",23,74.735432,"true")
+print("student")
+}
